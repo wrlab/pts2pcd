df4914012c9e0e0c6c9f2c032ca9c3e0091b5778
Yongjae Lee <yongjae.lee@wrl.onl>
2018-12-19 11:51:18 +0900

    Add binary output option

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const fs = require( 'fs' );
 const LineByLineReader = require('line-by-line');
 
+if (process.argv.length < 3) {
+
+	console.log( `Usage: node index.js filePath [--binary || --ascii]` );
+	process.exit(0);
+	
+}
+
 const inputPath = process.argv[2];
+const outputOpt = process.argv[3];
 const outputPath = inputPath + '.pcd';
 
 const lr = new LineByLineReader( inputPath );
@@ -17,7 +25,7 @@ let PCDHeader = {
     HEIGHT: 1,				// fixed as 1
     VIEWPOINT: '0 0 0 1 0 0 0',	// fixed as '0 0 0 1 0 0 0'
     POINTS: 0,				// equals with first line of pts file
-    DATA: 'ascii',			// fixed as 'ascii'
+    DATA: 'binary',			// 'ascii' or 'binary'
 }
 let lineCount = 0;
 
@@ -38,6 +46,17 @@ lr.on('line', function (line) {
 
 			PCDHeader.WIDTH = line[0];
 			PCDHeader.POINTS = line[0];
+			
+			// Set PCDHeader.DATA
+			switch (outputOpt) {
+			case undefined:
+			case '--binary':	break;				// Use default option: binary
+			case '--ascii':		PCDHeader.DATA = 'ascii'; break;
+			default:
+				console.log( `Invalid output option. It should be '--binary' or '--ascii'.` );
+				process.exit(1);
+			}
+			
 			writePCDHeader();
 
 		} else {
@@ -95,12 +114,35 @@ function writePoint( line ) {
 
 	let xyz = line.slice( 0, 3 );
 	let [r, g, b] = line.slice( 4, line.length ).map( element => parseInt(element) );
-
 	const intensity = (line[3] + 2048) / 4096	// normalize ranging in 0 to 1, unused property
-	const rgb = ((r & 0x0000ff) << 16) | ((g & 0x0000ff) << 8) | (b & 0x0000ff);
-	const point = xyz.join(' ') + ' ' + rgb + '\n';
 
-	os.write( point, 'ascii' );
+	if (PCDHeader.DATA === 'ascii') {
+	
+		const rgb = ((r & 0x0000ff) << 16) | ((g & 0x0000ff) << 8) | (b & 0x0000ff);
+		const point = xyz.join(' ') + ' ' + rgb + '\n';
+
+		os.write( point, 'ascii' );
+		
+	} else {
+	
+		const buffer = new ArrayBuffer( 12 + 3 );
+		const view = new DataView( buffer );
+		
+		// Write xyz
+		view.setFloat32( 0, parseFloat(xyz[0]), true );
+		view.setFloat32( 4, parseFloat(xyz[1]), true );
+		view.setFloat32( 8, parseFloat(xyz[2]), true );
+		
+		// Write color
+		view.setUint8( 12, r );
+		view.setUint8( 13, g );
+		view.setUint8( 14, b );
+		
+		// Write to file stream
+		const uint8Buffer = new Uint8Array( buffer );
+		os.write( uint8Buffer );
+			
+	}
 
 }
 
